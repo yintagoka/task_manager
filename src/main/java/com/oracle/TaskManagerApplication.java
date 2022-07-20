ea52c1907d521a7ca622c148c512af516ac443dc
Roy Krisnadi <skyglassio@gmail.com>
2022-07-20 16:26:37 +0800

    Need to add cors, so that the ui can access

--- src/main/java/com/oracle/TaskManagerApplication.java
+++ src/main/java/com/oracle/TaskManagerApplication.java
@@ -5,6 +5,11 @@ import com.oracle.resources.TaskResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TaskManagerApplication extends Application<TaskManagerConfiguration> {
 
@@ -27,8 +32,23 @@ public class TaskManagerApplication extends Application<TaskManagerConfiguration
                     final Environment environment) {
         final TaskResource resource = new TaskResource();
         final TaskHelathCheck healthCheck = new TaskHelathCheck();
+        configureCors(environment);
         environment.healthChecks().register("task", healthCheck);
         environment.jersey().register(resource);
     }
 
+    private void configureCors(Environment environment) {
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
+    }
 }
